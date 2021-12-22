bbed76ce4a64ef57d0978a9a7a72a9aff88996a7
VSADX <81491645+VSADX@users.noreply.github.com>
2021-12-22 10:48:06 -0600

    Update jsx-compiling-sw.js

--- jsx-compiling-sw.js
+++ jsx-compiling-sw.js
@@ -1,5 +1,5 @@
 /// <reference lib="webworker" />
-/// use id 0FBE34.
+/// use id F251CA.
 
 const waiter = Promise.all[
     new Promise(r => oninstall = r),
@@ -21,8 +21,7 @@ onmessage = async e => {
 }
 
 onfetch = e =>
-    !e.request.url.endsWith("jsx") ? 0 : 
-    e.respondWith(respondable(e.request))
+    e.request.url.endsWith("jsx") && e.respondWith(respondable(e.request))
 
 
 async function respondable(request = new Request) {
@@ -30,21 +29,44 @@ async function respondable(request = new Request) {
     const response = new Response(new Blob([jsx(script, false)]))
     const { headers: h, body, status, statusText } = response
     const headers = new Headers(h)
-    headers.set('content-type', 'application/javascript; charset=UTF-8')
+    headers.set('Content-Type', 'application/javascript; charset=UTF-8')
     return new Response(body, { headers, status, statusText })  
 }
 
 const matcher = {
     "=>({": false,
     "=>{": false,
+    "({": false,
+    "})": false,
+    "${": false,
+    "var{": false,
+    "let{": false,
+    "const{": false,
     "(<": "( /* from jsx */ html`<",
     ">)": ">` /* from jsx */ )",
+    "{{": "${{",
     "{": "${",
-    "${": false,
+    "}": false,
     // // uncomment to enable JSX-extended-syntax
     // "{": "${useHtml(() => ",
     // "}": ")}",
 }
+const order = [
+    /\({/, //               ({
+    /}\)/, //               })
+    /{/, //                 {
+    /}/, //                 }
+    /\((\s*?)</, //         (<
+    />(\s*?)\)/, //         >)
+    /{{/, //                {{
+    /var(\s*?){/, //        var {
+    /let(\s*?){/, //        let {
+    /const(\s*?){/, //      const {
+    /${/, //                ${
+    /=>(\s*?){/, //         => {
+    /=>(\s*?)\((\s*?){/, // => ({
+]
+const rex = new RegExp(order.map(r => r.source).join("|"),"gm")
 
 function jsx(string_component = "", should_compile = true, compiler = eval) {
 
@@ -58,7 +80,7 @@ function jsx(string_component = "", should_compile = true, compiler = eval) {
         const html = ending[0]
         
         ending[0] = html
-            .replace(/(\((\s*?)<|${|{|>(\s*?)\)|=>(\s*?){|=>(\s*?)\((\s*?){)/gm, 
+            .replace(rex, 
                 match => matcher[match.replace(/\s/gm, "")] || match)
             .replace(/\<([A-Z][a-zA-Z.]+)/g,"<${$1}")
             .replace(/\<\/([A-Z][a-zA-Z.]+)/g,"</${$1}")
